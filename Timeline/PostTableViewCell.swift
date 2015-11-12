dda6bac559e060b1c98ae5cf83b21d5623e908ca
Andrew Porter <haporter@me.com>
2015-11-11 18:49:37 -0700

    test run
    Profile edit button segues properly but does not populate the text fields with current user info.
    - doesn't hid any text fields
    - sign up button doesn't change to "update"
    
    tap friend doesn't update profile view with friend
    -should hide edit button when viewing friend profile

--- Timeline/PostTableViewCell.swift
+++ Timeline/PostTableViewCell.swift
@@ -39,3 +39,4 @@ class PostTableViewCell: UITableViewCell {
     }
 
 }
+
--- Timeline/TimelineTableViewController.swift
+++ Timeline/TimelineTableViewController.swift
@@ -14,6 +14,7 @@ class TimelineTableViewController: UITableViewController {
         loadTimelineForUser(UserController.sharedController.currentUser)
     }
     
+    
     var posts: [Post] = []
 
     override func viewDidLoad() {
