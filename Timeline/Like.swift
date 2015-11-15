9c3ee03a65f2b503089d0b57d160612ad689428e
Andrew Porter <haporter@me.com>
2015-11-15 12:49:14 -0700

    Adopted the FirebaseType protocol in the model objects
    The program is now riddled with errors, after adopting the protocol in the model objects. Every model object as an error that it does not conform to the protocol.

--- Timeline/Like.swift
+++ Timeline/Like.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct Like: Equatable, FirebaseType {
+struct Like: FirebaseType {
     
     private let kPost = "post"
     private let kUsername = "username"
@@ -22,6 +22,15 @@ struct Like: Equatable, FirebaseType {
     
     var jsonValue: [String:AnyObject] {
         return [kUsername: username, kPost: postIdentifier]
+    }
+    
+    init?(json: [String : AnyObject], identifier: String) {
+        guard let postIdentifier = json[kPost] as? String,
+              let username = json[kUsername] as? String else { return nil }
+        
+        self.postIdentifier = postIdentifier
+        self.username = username
+        self.identifier = identifier
     }
     
     init(username: String, postIdentifier: String, identifier: String? = nil) {
--- Timeline/Post.swift
+++ Timeline/Post.swift
@@ -10,21 +10,62 @@ import Foundation
 
 struct Post: Equatable, FirebaseType {
     
-    let imageEndPoint: String
+    private let kImageEndpoint = "image"
+    private let kCaption = "caption"
+    private let kUsername = "username"
+    private let kComments = "comments"
+    private let kLikes = "likes"
+    
+    let imageEndpoint: String
     let caption: String?
     let username: String
     let comments: [Comment]
     let likes: [Like]
     let identifier: String?
+    var endpoint: String {
+        return "posts"
+    }
     
-    init(imageEndPoint: String, caption: String? = nil, username: String, comments: [Comment] = [], likes: [Like] = [], identifier: String? = nil) {
+    var jsonValue: [String: AnyObject] {
+        var json: [String: AnyObject] = [kUsername : username, kImageEndpoint : imageEndpoint, kComments : comments.map({$0.jsonValue}), kLikes : likes.map({$0.jsonValue})]
         
-        self.imageEndPoint = imageEndPoint
+        if let caption = caption {
+            json.updateValue(caption, forKey: kCaption)
+        }
+        
+        return json
+    }
+    
+    init(imageEndpoint: String, caption: String? = nil, username: String, comments: [Comment] = [], likes: [Like] = [], identifier: String? = nil) {
+        
+        self.imageEndpoint = imageEndpoint
         self.caption = caption
         self.username = username
         self.comments = comments
         self.likes = likes
         self.identifier = identifier
+    }
+    
+    init?(json: [String : AnyObject], identifier: String) {
+        guard let imageEndpoint = json[kImageEndpoint] as? String,
+            let username = json[kUsername] as? String else { return nil }
+        
+        self.imageEndpoint = imageEndpoint
+        self.caption = json[kCaption] as? String
+        self.username = username
+        self.identifier = identifier
+        
+        if let commentDictionaries = json[kComments] as? [String: AnyObject] {
+            self.comments = commentDictionaries.flatMap({Comment(json: $0.1 as! [String : AnyObject], identifier: $0.0)})
+        } else {
+            self.comments = []
+        }
+        
+        if let likeDictionaries = json[kLikes] as? [String: AnyObject] {
+            self.likes = likeDictionaries.flatMap({Like(json: $0.1 as! [String : AnyObject], identifier: $0.0)})
+        } else {
+            self.likes = []
+        }
     }
 }
 
