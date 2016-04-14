7d54af51b9d1d92ca59208622dbf11b6ddca100d
Andrew Porter <haporter@me.com>
2016-04-14 17:30:02 -0600

    finish part 3

--- Timeline/AddPhotoTableViewController.swift
+++ Timeline/AddPhotoTableViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class AddPhotoTableViewController: UITableViewController {
+class AddPhotoTableViewController: UITableViewController, UINavigationControllerDelegate {
     
     // MARK: - Properties
-    
+    var image: UIImage?
+    var caption: String?
     
     // MARK: - Outlets
     @IBOutlet var captionTextField: UITextField!
@@ -21,5 +22,71 @@ class AddPhotoTableViewController: UITableViewController {
         super.viewDidLoad()
 
     }
+    
+    // MARK: - Actions
+    @IBAction func addPhotoButtonTapped() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        let alert = UIAlertController(title: "Select Photo Location", message: nil, preferredStyle: .ActionSheet)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary) {
+            alert.addAction(UIAlertAction(title: "Camera", style: .Default, handler: { (_) in
+                imagePicker.sourceType = .Camera
+                self.presentViewController(imagePicker, animated: true, completion: nil)
+            }))
+        }
+        alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func postButtonTapped() {
+    
+        if let image = self.image {
+            PostController.addPost(image, caption: self.caption, completion: { (success, post) in
+                if post != nil {
+                    self.dismissViewControllerAnimated(true, completion: nil)
+                }
+            })
+        } else {
+            let failedAlert = UIAlertController(title: "ERROR", message: "OOPS it seems there was a problem selecting an image, please try again.", preferredStyle: .Alert)
+            let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+            failedAlert.addAction(action)
+            presentViewController(failedAlert, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func cancelButtonTapped() {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
 
+// MARK: - Extensions
+
+extension AddPhotoTableViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+        
+            self.image = image
+            postButton.setTitle("", forState: .Normal)
+            postButton.setBackgroundImage(image, forState: .Normal)
+        }
+    }
+}
+
+extension AddPhotoTableViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        if let text = textField.text {
+            self.caption = text
+        }
+        
+        textField.resignFirstResponder()
+        return true
+    }
 }
